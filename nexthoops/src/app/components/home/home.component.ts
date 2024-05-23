dc968b9cf03f9455880743e2ea26d0d860cf6de6
Eloy Perez <titoeloc@gmail.com>
2024-05-24 00:13:17 +0200

    Cambios de estilo, y algunos adds

--- nexthoops/src/app/components/home/home.component.ts
+++ nexthoops/src/app/components/home/home.component.ts
@@ -1,40 +1,12 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements AfterViewInit {
-  images: string[] = [
-    '../../assets/img/i1.webp',
-    '../../assets/img/i2.webp',
-    '../../assets/img/i3.webp',
-    '../../assets/img/i4.webp',
-    '../../assets/img/i5.webp',
-    '../../assets/img/i6.webp',
-  ];
-  currentImageIndex = 0;
-
-  constructor() {}
-
-  ngAfterViewInit(): void {}
-
-  // ngAfterViewInit(): void {
-  //   this.iniciarTimer();
-  // }
-
-  // iniciarTimer() {
-  //   setInterval(() => {
-  //     this.showNextImage();
-  //   }, 3000);
-  // }
-
-  // showNextImage() {
-  //   this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
-  // }
-}
+export class HomeComponent {}
